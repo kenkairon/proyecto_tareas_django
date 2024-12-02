38b6514d890d177f9209cd24f82065cda7442cb8
kenkairon <ingeinfo2006@gmail.com>
2024-12-02 12:13:27 -0300

    Agregamos el formulario crear tareas

--- proyecto/baseapp/urls.py
+++ proyecto/baseapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ListasPendientes, DetalleTarea, CrearTarea
 
 urlpatterns = [
-    path('',ListasPendientes.as_view(), name="pendientes"),
+    path('',ListasPendientes.as_view(), name="tareas"),
     path('tarea/<int:pk>', DetalleTarea.as_view(), name="tarea"),
     path('crear-tarea/', CrearTarea.as_view(), name="crear-tarea"),
 ]
